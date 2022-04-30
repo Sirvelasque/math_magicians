27c6c17028942241a3decf08e946fb430a7591bf
Sirvelasque <velasquezwalter10@yahoo.es>
2022-04-30 17:02:00 -0600

    Create Nav Bar and route it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/navBar';
 import Calculator from './Components/calculator';
 
 class App extends React.PureComponent {
   render() {
     return (
       <div>
-        <Calculator />
+        <HashRouter>
+          <NavBar />
+          <Routes>
+            <Route path="Calculator" exacto element={<Calculator />} />
+          </Routes>
+
+        </HashRouter>
       </div>
     );
   }
